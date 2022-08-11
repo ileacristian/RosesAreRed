fbcb3c0f630722a78fff309502bdc876801ebb1c
Ilea Cristian <ileacristian@gmail.com>
2022-08-11 14:30:20 +0300

    initial commit

---
+++ RosesAreRed/RosesAreRedApp.swift
@@ -0,0 +1,17 @@
+//
+//  RosesAreRedApp.swift
+//  RosesAreRed
+//
+//  Created by Cristian Ilea on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct RosesAreRedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
