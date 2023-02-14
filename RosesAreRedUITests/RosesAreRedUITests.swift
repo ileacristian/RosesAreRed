54b2f25cc5125130eaa7b3b2a308a54e3f7d7c0a
Cristian Ilea <cristian.ilea@paddypower.com>
2023-02-14 16:01:16 +0200

    Added UI tests

---
+++ RosesAreRedUITests/RosesAreRedUITests.swift
@@ -0,0 +1,24 @@
+//
+//  RosesAreRedUITests.swift
+//  RosesAreRedUITests
+//
+//  Created by Cristian Ilea on 14.02.2023.
+//
+
+import XCTest
+
+class RosesAreRedUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
